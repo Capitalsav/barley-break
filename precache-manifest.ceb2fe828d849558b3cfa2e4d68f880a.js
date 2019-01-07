e0a962d9e54e85db32afed28b1c8e0c58b1d2109
Anton <capitalsav@gmail.com>
2019-01-07 23:13:13 +0200

    Updates

---
+++ precache-manifest.ceb2fe828d849558b3cfa2e4d68f880a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6159d5dc9beda2e8712c",
+    "url": "/barley-break/static/js/runtime~main.6159d5dc.js"
+  },
+  {
+    "revision": "ee975deb9f98ae6f7e8e",
+    "url": "/barley-break/static/js/main.ee975deb.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/barley-break/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "ee975deb9f98ae6f7e8e",
+    "url": "/barley-break/static/css/main.e4334428.chunk.css"
+  },
+  {
+    "revision": "516a8151d69ac26cc949241499cf2af6",
+    "url": "/barley-break/index.html"
+  }
+];
